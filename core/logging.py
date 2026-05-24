da491f8178102ee348b15162ea13f37a9b0d878e
gum1x <gum1x@users.noreply.github.com>
2026-05-23 23:28:15 -0400

    patching logger

--- core/logging.py
+++ core/logging.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 import structlog
 
 
@@ -19,7 +20,7 @@ def setup_logging(log_level: str = "INFO", json_output: bool = True):
     structlog.configure(
         processors=processors,
         wrapper_class=structlog.make_filtering_bound_logger(
-            structlog.get_level_from_name(log_level)
+            logging.getLevelName(log_level.upper())
         ),
         context_class=dict,
         logger_factory=structlog.PrintLoggerFactory(file=sys.stderr),
